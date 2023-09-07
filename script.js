0dc29e1145dea031a9a9c850152dfba6971ee434
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-09-07 21:45:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const content = document.querySelector(".content");
 // Function to create and animate particles
 function createParticle(x, y) {
   const particle = document.createElement("div");
+  particle.classList.add("particle");
